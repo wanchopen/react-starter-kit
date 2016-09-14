1b3e672708e5298e07c3bab759851b2a61b13b07
Roman Zagumennov <wanchopen@gmail.com>
2016-09-15 01:31:52 +0300

    fixed minor notes after review

--- src/routes/goosepage/Goosepage.js
+++ src/routes/goosepage/Goosepage.js
@@ -1,16 +1,16 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Goosepage.css'
+import s from './Goosepage.css';
 
 function Goosepage({ title }) {
-    return (
-        <div className={s.root}>
-            <div className={s.container}>
-                <h1>{title}</h1>
-                <p>...</p>
-            </div>
-        </div>
-        );
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <h1>{title}</h1>
+        <p>...</p>
+      </div>
+    </div>
+  );
 }
 
 Goosepage.contextTypes = { setTitle: PropTypes.func.isRequired };
--- src/routes/goosepage/index.js
+++ src/routes/goosepage/index.js
@@ -4,14 +4,11 @@ import Goosepage from './Goosepage';
 const title = 'The Goose Page';
 
 export default {
-
-    path: '/goosepage',
-
-        action() {
-        return {
-            title,
-            component: <Goosepage title={title} />,
-        };
-    },
-
+  path: '/goosepage',
+  action() {
+    return {
+      title,
+      component: <Goosepage title={title} />,
+    };
+  },
 };
